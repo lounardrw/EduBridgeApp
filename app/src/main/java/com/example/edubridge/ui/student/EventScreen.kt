f139d7d068ef6aca756d28eee0f3e3d58ec92886
Monserrat <monserrat_rh1@tesch.edu.mx>
2025-12-03 16:34:20 -0600

    prueba

--- app/src/main/java/com/example/edubridge/ui/student/EventScreen.kt
+++ app/src/main/java/com/example/edubridge/ui/student/EventScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+//prueba
 @Composable
 fun EventsScreen() {
     // TODO: Aquí se mostrará el calendario y los avisos.
