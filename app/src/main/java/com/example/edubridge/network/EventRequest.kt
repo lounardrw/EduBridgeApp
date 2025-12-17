6345231760ed45deb4e471aa0263990e013a8681
Monserrat <monserrat_rh1@tesch.edu.mx>
2025-12-17 14:21:06 -0600

    Cambios segunda parte (Mk)

--- app/src/main/java/com/example/edubridge/network/EventRequest.kt
+++ app/src/main/java/com/example/edubridge/network/EventRequest.kt
@@ -1,2 +1,9 @@
 package com.example.edubridge.network
 
+data class EventRequest(
+    val id: Int? = null,
+    val title: String,
+    val description: String?,
+    val date: String,
+    val created_by: Int
+)
